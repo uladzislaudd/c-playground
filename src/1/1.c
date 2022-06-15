12db63d4157cfae42e616b5c3c435546ebd2ec82
uladzislaudd <106530828+uladzislaudd@users.noreply.github.com>
2022-06-15 13:44:24 +0300

    test task 1 (#1)

---
+++ src/1/1.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int STATUS;
+
+enum {
+    ERR_SUCCESS = 0,
+    ERR_FAILURE,
+    ERR_ARGS_INVALID,
+    ERR_NO_MEMORY,
+};
+
+typedef struct list_s
+{
+    struct list_s *next; /* NULL for the last item in a list */
+    int data;
+} list_t;
+
+/* Counts the number of items in a list. */
+size_t count_list_items(const list_t *head)
+{
+    size_t rv = 0;
+    const list_t *i = head;
+
+    while (i != NULL) {
+        i = i->next;
+        rv++;
+    }
+
+    return rv;    
+}
+
+/* Inserts a new list item after the one specified as the argument. */
+STATUS insert_next_to_list(list_t *item, int data) {
+    struct list_s *new = NULL;
+
+    if (item == NULL) {
+        return ERR_ARGS_INVALID;
+    }
+
+    new = malloc(sizeof(list_t));
+    if (new == NULL) {
+        return ERR_NO_MEMORY;
+    }
+
+    new->next = item->next;
+    new->data = data;
+    item->next = new;
+
+    return ERR_SUCCESS;
+}
+
+/* Removes an item following the one specificed as the argument. */
+STATUS remove_next_from_list(list_t *item) {
+    struct list_s *new_next = NULL, *to_delete = NULL;
+
+    if (item == NULL) {
+        return ERR_ARGS_INVALID;
+    }
+
+    to_delete = item->next;
+    if (to_delete != NULL) {
+        new_next = to_delete->next;
+    }
+
+    item->next = new_next;
+
+    if (to_delete != NULL) {
+        free(to_delete);
+    }
+
+    return ERR_SUCCESS;
+}
+
+/* Returns item data as text. */
+STATUS item_data(const list_t *list, char *buf, size_t size)
+{
+    STATUS rv = ERR_FAILURE;
+    int rvs;
+
+    if (list == NULL) {
+        return ERR_ARGS_INVALID;
+    }
+
+    rvs = snprintf(buf, size, "%d", list->data);
+    return rvs > size ? ERR_NO_MEMORY
+                      : (rvs < 0 ? ERR_FAILURE
+                                 : ERR_SUCCESS);
+}
---
+++ src/1/5/src/core/5.c
@@ -0,0 +1,6 @@
+#include "5.h"
+
+int main(int argc, char const *argv[])
+{
+    return fn5();
+}
